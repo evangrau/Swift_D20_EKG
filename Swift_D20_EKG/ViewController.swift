aa8daa40ea4a224726d580c86b8834fb7ca305f8
Evan Grau <ekg20b@acu.edu>
2022-11-15 09:19:05 -0600

    initial commit

--- Swift_D20_EKG/ViewController.swift
+++ Swift_D20_EKG/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var diceImageView: UIImageView!
+    @IBOutlet var criticalLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonGotPressed() {
+        rollDice()
+    }
+    
+    func rollDice() {
+        print("We rollin' dem dice")
+        
+        let rolledNumber = Int.random(in: 1...20)
+        let imageName = "d\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+    }
 }
 
