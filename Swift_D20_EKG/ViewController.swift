ac31c4f03faab539c5248e38d6dd02aad87a793b
whatissatire <ekg20b@acu.edu>
2022-11-15 08:21:25 -0600

    Initial Commit

---
+++ Swift_D20_EKG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_D20_EKG
+//
+//  Created by Evan Grau on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
